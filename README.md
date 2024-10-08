91fb30ee29e85ffc220b29f3739848e3983f0463
dpineda6988 <daniel.m.pineda@gmail.com>
2024-10-08 01:51:07 -0400

    Updated files

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # belly-button-challenge
+# BellyButtonChallenge
+
+An interactive dashboard built using HTML, Javascript and the D3.js library that explores NC State's Public Science Lab Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels.
+
+<strong>Project can also be viewed as a GitHub Page at the following link: <br>
+
+</strong>
+
+## Description
+
+The Public Science Lab at NC State launched a study in January 2011 to investigate the microbes inhabiting our navels and the factors that might influence the microscopic life calling this protected, moist patch of skin home.  The Belly Button Biodiversity project began locally in the Raleigh-Durham area but was eventually opened up to people all over North America due to popular demand.
+
+The resulting dataset revealed that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+This project uses a portion of the comprehensive dataset to create an interactive dynamic dashboard that highlights some key metadata on the each study subject and data visualizations  portrays the that I've  creates an interactive dashboard that can be opened up with any
+
+This project uses a combination of HTML, JavaScript, and the D3.js library to present a portion of the dataset in the form of an interactive dynamic dashboard.  This dashboard displays data for individual study participants which a user can select from a drop down list based upon study subject ID.  Making a selection will display some topline metadata on the selected subject (such as participant ID, ethnicity, gender, age, etc.) as well as two graph visualizations that display the folowing:
+* A horizontal bar chart that displays the OTUs with the highest measured number in the selected subject (max top 10)
+* A bubble chart that also visually displays the number of bacteria per OTU found in the subject, where the where size of the bubble for any given OTU is relative to the number of that OTU present in the subject
+
+### Screenshot of Output
+![alt text](screenshot.png)
+
+
+
+### Dependencies
+* Any web browser program that will open 'index.html' (Google Chrome is recommended)
+* An active internet connection so taht 'index.html' can access the dataset url and the necessary libraries.
+
+### Installing & Execution
+The repository files can be downloaded to open 'index.html'  Note that since 'index.html' contains relative file paths, it is important that the file locations within the BellyButtonChallenge directory are not changed in order to execute properly.
+
+<strong>Note</strong>: file 'samples.json' contains a copy of the json data that is used in the dashboard but is only provided for reference and is not needed to execute the dashboard coded in 'index.html'
+
+## Authors
+
+Daniel Pineda
+
+## Acknowledgments
+BellyButtonChallenge was created as an assignment for the University of California, Irvine Data Analytics Bootcamp - June 2024 Cohort under the instruction and guidance of Melissa Engle (Instructor) and Mitchell Stone (TA).
+The practical exercises and coding examples demonstrated through the bootcamp helped inform and inspire the code for this project.
+
+Additionally, the following resources were used for further reference:
+
+* Xpert Learning Assistant - UCI's AI-powered learning assistant - referenced for how loop through filtered d3.json data ('app.js' - lines 18-20)
+* [Plotly JavaScript Open Source Graphing Library](https://plotly.com/javascript/) - referenced for syntax and structure of creating the bubble graph and bar graph ('app.js - lines 42-116)
--- static/js/app.js
+++ static/js/app.js
@@ -1,53 +1,117 @@
-// Build the metadata panel
+// Fucntion to build the metadata panel
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
-    // get the metadata field
-
+    // Get the metadata field
+    let metadataField = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
-
+    let targetObjectMetadata  = metadataField.filter(x => x.id == sample);
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let metadataDisplay = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+    metadataDisplay.html("");
 
-
-    // Inside a loop, you will need to use d3 to append new
-    // tags for each key-value in the filtered metadata.
-
+    // Inside a loop, you will need to use d3 to append new tags for each key-value in the filtered metadata.
+    // Loop through the filtered metadata object 'targetObjectMetadata' and append the key and value as an html tag to the metadata panel
+    for ([key, value] of Object.entries(targetObjectMetadata[0])) {
+      metadataDisplay.append("html").text(`${key.toUpperCase()}: ${value}`);
+  }
   });
 }
 
-// function to build both charts
+// Function to build both charts
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    let sampleField = data.samples;
 
     // Filter the samples for the object with the desired sample number
+    let targetObjectSamples = sampleField.filter(x => x.id == sample);
 
-
-    // Get the otu_ids, otu_labels, and sample_values
-
+    // Get the otu_ids, otu_labels, and sample_values and store them in variables
+    let otuIDs = targetObjectSamples[0].otu_ids;
+    let otuLabels = targetObjectSamples[0].otu_labels;
+    let sampleValues = targetObjectSamples[0].sample_values;
 
     // Build a Bubble Chart
+    // Create trace object for bubble chart, using otu_ids for x-values and marker colors,
+    // sample_values for y-values and marker size, and otu_lables as the hover-text values
+    let bubbleTrace = {
+      x: otuIDs,
+      y: sampleValues,
+      name: 'Bubble Chart',
+      text: otuLabels,
+      mode: 'markers',
+      marker: {
+        color: otuIDs,
+        colorscale: 'Earth',
+        size: sampleValues,
+        sizeref: 1, // set sizeref so that marker size will scale based on sampleValues
+        sizemode: 'diameter' // marker size will be based on marker diameter
+      }
+    };
 
+    // Set trace data as an array
+    bubbleData = [bubbleTrace];
+
+    // Set layout of the bubble chart
+    bubbleLayout = {
+      title: 'Bacteria Culture Per Sample',
+      showlegend: false,
+      xaxis: {
+        title: 'OTU ID'
+      },
+      yaxis: {
+        title: 'Number of Bacteria'
+      },
+      height: 800
+    }
 
     // Render the Bubble Chart
-
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+    // Build a Bar Chart - Don't forget to slice and reverse the input data appropriately
 
+    // Create a list/array to store the list of otu ID strings
+    let otuIDLabels = [];
+    
+    // Since the sample values are alreayd in descending order, slice the first 10 available values for sampleValues
+    // and their corresponding otu IDs.  Reverse the sliced input data for Plotly graphing
+    let topTenSamples = sampleValues.slice(0,10).reverse();
+    let topTenOTUs = otuIDs.slice(0,10).reverse();
+    
+    // Map the otu_ids as a formatted text string to the array 'otuIDLabels'
+    topTenOTUs.map(otu => otuIDLabels.push(`OTU ${otu}`));
 
-    // Build a Bar Chart
-    // Don't forget to slice and reverse the input data appropriately
+    // Create trace object for horizontal bar chart, using the stored otu ID strings as y-axis labels,
+    // sample_values for the bar chart values, and otu_lables as the hover-text values    
+    barTrace = {
+      x: topTenSamples,
+      y: otuIDLabels,
+      name: 'Bar Chart',
+      text: otuLabels,
+      orientation: 'h',
+      type: 'bar'
+    };
 
+    // Set trace data as an array
+    barData = [barTrace];
+
+    // Set layout of the horizontal bar chart    
+    barLayout = {
+      title: 'Top 10 Bacteria Cultures Found',
+      showlegend: false,
+      xaxis: {
+        title: 'Number of Bacteria'
+      }
+    };
 
     // Render the Bar Chart
-
+    Plotly.newPlot("bar", barData, barLayout);    
   });
 }
 
@@ -56,28 +120,34 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
-
+    let dropdownMenu = d3.select("#selDataset");
     // Use the list of sample names to populate the select options
-    // Hint: Inside a loop, you will need to use d3 to append a new
-    // option for each sample name.
-
+    // Inside a loop, use d3 to append a new option for each sample name with a matching 'value' attribute
+    for (let i = 0; i < names.length; i++) {
+      dropdownMenu.append("option").text(names[i]).attr("value", names[i]);
+    }
 
     // Get the first sample from the list
+    let firstSample = names[0];
 
+    // Build charts and metadata panel with the first sample by calling the corresponding functions
+    buildMetadata(firstSample);
+    buildCharts(firstSample);
 
-    // Build charts and metadata panel with the first sample
-
+    // When a new sample is selected, call the optionChanged function to update the dashboard, passing the new
+    // selected value of the dropdown menu
+    d3.select("#selDataset").on("change", optionChanged(dropdownMenu.property("value")));
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildMetadata(newSample);
+  buildCharts(newSample);
 }
 
 // Initialize the dashboard
